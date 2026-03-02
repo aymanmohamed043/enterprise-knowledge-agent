97dea2a5daa1ec6fc7881203f3ba7c607e1c3dea
ayman <ayman.mohamed141163@el-eng.menofia.edu.eg>
2026-03-03 01:03:30 +0200

    adding some notes and handle reset of project

--- app/db/db_main.py
+++
@@ -1,9 +0,0 @@
-from app.db.base import Base
-from app.db.database import engine
-from app.db import models
-
-
-if __name__ == "__main__": 
-    # Create all tables in the database
-    Base.metadata.create_all(bind=engine)
-    print("All tables created successfully!")
